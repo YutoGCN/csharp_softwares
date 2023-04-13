3f32175ee7d88474840ae7789aa8b50686bc3a10
YutoGCN <61838168+YutoGCN@users.noreply.github.com>
2023-04-13 20:55:01 +0900

    マンデルブロ集合を描画するc#ソリューションを追加

---
+++ 230413_plt_test/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace _230413_plt_test
+{
+    partial class plt_test
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.test_button = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // test_button
+            // 
+            this.test_button.Location = new System.Drawing.Point(12, 12);
+            this.test_button.Name = "test_button";
+            this.test_button.Size = new System.Drawing.Size(75, 23);
+            this.test_button.TabIndex = 0;
+            this.test_button.Text = "reset";
+            this.test_button.UseVisualStyleBackColor = true;
+            this.test_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(626, 444);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // plt_test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.test_button);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "plt_test";
+            this.Text = "mandelplt";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button test_button;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 230413_plt_test/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _230413_plt_test
+{
+    public partial class plt_test : Form
+    {
+        public plt_test()
+        {
+            InitializeComponent();
+            this.pictureBox1.MouseWheel += PictureBox1_MouseWheel;
+        }
+
+        private void PictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Point sp = System.Windows.Forms.Cursor.Position;
+            System.Drawing.Point cp = this.PointToClient(sp);
+            int x = cp.X;
+            int y = cp.Y;
+
+            Graphics g = pictureBox1.CreateGraphics();
+            int width = (int)g.VisibleClipBounds.Width;
+            int height = (int)g.VisibleClipBounds.Height;
+
+            if (e.Delta > 0)
+            {
+                mandelpltsetparam(x_center += step * (x - (width / 2)), y_center += step * (y - (height / 2)), step / 1.2);
+            }
+            else
+            {
+                mandelpltsetparam(x_center += step * (x - (width / 2)), y_center += step * (y - (height / 2)), step / 0.8);
+            }
+        }
+
+        private double x_center;
+        private double y_center;
+        private double step = 2.0/540;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mandelpltreset();
+        }
+
+        private void mandelpltsetparam(double _x_center, double _y_center, double _step)
+        {
+            x_center = _x_center;
+            y_center = _y_center;
+            step = _step;
+            Graphics g = pictureBox1.CreateGraphics();
+
+            int width = (int)g.VisibleClipBounds.Width;
+            int height = (int)g.VisibleClipBounds.Height;
+
+            mandelplt(g, width, height, x_center, y_center, step);
+        }
+
+        public void Mandelpltreset()
+        {
+            mandelpltsetparam(-0.5, 0, 2.0 / 540);
+        }
+
+        private void mandelplt(Graphics g,int width,int height,double x_center, double y_center,double step)
+        {
+            Bitmap picData = new Bitmap(width, height);
+
+            double xmin = x_center -width * step/2;
+            double ymin = y_center -height * step/2;
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    // 複素数の計算
+                    double a = xmin + x * step;
+                    double b = ymin + y * step;
+                    double ca = a;
+                    double cb = b;
+                    int n = 0;
+                    while (n < 255)
+                    {
+                        double aa = a * a - b * b;
+                        double bb = 2 * a * b;
+                        a = aa + ca;
+                        b = bb + cb;
+                        if (a * a + b * b > 4)
+                        {
+                            break;
+                        }
+                        n++;
+                    }
+
+                    // 色の設定
+                    picData.SetPixel(x, y, Color.FromArgb(n, n, n ));
+                }
+            }
+
+            g.DrawImage(picData, 0, 0);
+            g.Dispose();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Point sp = System.Windows.Forms.Cursor.Position;
+            System.Drawing.Point cp = this.PointToClient(sp);
+            int x = cp.X;
+            int y = cp.Y;
+
+            Graphics g = pictureBox1.CreateGraphics();
+            int width = (int)g.VisibleClipBounds.Width;
+            int height = (int)g.VisibleClipBounds.Height;
+
+            mandelpltsetparam(x_center += step * (x-width/2), y_center += step * (y-height/2), step / 1.2);
+        }
+    }
+}
